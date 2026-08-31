e7aaf5f536ada5feaabcff2c4aa9b615c747f47d
DROYER Florent <flo.droyer@gmail.com>
2026-08-31 09:27:23 +0200

    feat(game): export level solver API

--- packages/game/src/index.ts
+++ packages/game/src/index.ts
@@ -1,3 +1,5 @@
 export type { Form, Level, Position, Tile } from '@duality/level-format';
 export { LevelRunner } from './LevelRunner';
 export type { Direction, GameState } from './LevelRunner';
+export { solveLevel, replay } from './LevelSolver';
+export type { SolverCommand, SolverResult } from './LevelSolver';
